c1aa59ffea64bfc2d964b5a98751bcdf5fc664ba
HenriqueZuffo <henriquezuffo2@gmail.com>
2022-06-12 14:59:10 -0300

    Code

---
+++ src/app/components/pages/home/home.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { Moment } from 'src/app/interfaces/Moment';
+import { MomentService } from 'src/app/services/moment.service';
+import { environment } from 'src/environments/environment';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+
+  allMoments!: Moment[];
+  moments!: Moment[];
+  baseApiUrl = environment.baseApiUrl;
+
+  faSearch = faSearch;
+  seachTerm!: string;
+
+
+  constructor(private momentService: MomentService) { }
+
+  ngOnInit(): void {
+
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
+      });
+
+      this.allMoments = data;
+      this.moments = data;
+    })
+  }
+
+
+  search(e: Event): void{
+    const target = e.target as HTMLInputElement
+    const value = target.value
+
+    this.moments = this.allMoments.filter((moment) => {
+      return moment.title.toLowerCase().includes(value)
+    })
+  }
+}
